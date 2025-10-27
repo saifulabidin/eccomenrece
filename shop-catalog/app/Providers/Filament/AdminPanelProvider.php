cb8238d1db4729d1ad57495dc93a6374384b2afe
Saiful Abidin <contact@sabidzpro.is-a.dev>
2025-10-27 12:36:18 +0700

    fix: Remove deprecated login route name from AdminPanelProvider

--- shop-catalog/app/Providers/Filament/AdminPanelProvider.php
+++ shop-catalog/app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,6 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->loginRouteSlug('login')
-            ->loginRouteName('admin.login')
             ->brandName('Admin Katalog Toko')
             ->favicon(asset('favicon.ico'))
             ->colors([
